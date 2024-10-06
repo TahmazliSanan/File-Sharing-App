9762c1fa825ef8aaaa557c3dd43eaf44c572ca5c
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-06 23:34:32 +0400

    Create response: ApiResponse

---
+++ src/main/java/org/pronet/app/responses/ApiResponse.java
@@ -0,0 +1,12 @@
+package org.pronet.app.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private Boolean success;
+    private String message;
+    private Object data;
+}
