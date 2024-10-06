7642f2d5ecc7979206ca6685d87c276e51d3a3ea
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-06 18:34:10 +0400

    Create overload constructor

--- src/main/java/org/pronet/app/dtos/UserDto.java
+++ src/main/java/org/pronet/app/dtos/UserDto.java
@@ -8,5 +8,13 @@ public class UserDto {
     private Long id;
     private String fullName;
     private String username;
+    private String password;
     private Role role;
+
+    public UserDto(Long id, String fullName, String username, Role role) {
+        this.id = id;
+        this.fullName = fullName;
+        this.username = username;
+        this.role = role;
+    }
 }
