2532d74023ee288067107147de12e2421434249d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-06 17:33:27 +0400

    Create project using Spring Initializr

---
+++ src/main/java/org/pronet/app/CloudBuddyApplication.java
@@ -0,0 +1,13 @@
+package org.pronet.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBuddyApplication.class, args);
+    }
+
+}
