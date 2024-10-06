1d12ea65631db163248194148046a513689cbb3b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-06 23:33:55 +0400

    Add annotations

--- src/main/java/org/pronet/app/dtos/UserDto.java
+++ src/main/java/org/pronet/app/dtos/UserDto.java
@@ -1,14 +1,27 @@
 package org.pronet.app.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.pronet.app.enums.Role;
 
 @Data
 public class UserDto {
     private Long id;
+
+    @NotNull(message = "Full name cannot be null!")
+    @NotBlank(message = "Full name cannot be empty!")
     private String fullName;
+
+    @NotNull(message = "Username cannot be null!")
+    @NotBlank(message = "Username cannot be empty!")
     private String username;
+
+    @NotNull(message = "Password cannot be null!")
+    @NotBlank(message = "Password cannot be empty!")
     private String password;
+
+    @NotNull(message = "Role cannot be empty or null!")
     private Role role;
 
     public UserDto(Long id, String fullName, String username, Role role) {
