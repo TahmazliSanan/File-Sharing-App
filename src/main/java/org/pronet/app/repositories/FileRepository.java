7ab1afe5c467758e4204b5054e691d10760b54c5
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-07 15:06:21 +0400

    Create repository: FileRepository

---
+++ src/main/java/org/pronet/app/repositories/FileRepository.java
@@ -0,0 +1,9 @@
+package org.pronet.app.repositories;
+
+import org.pronet.app.entities.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+}
