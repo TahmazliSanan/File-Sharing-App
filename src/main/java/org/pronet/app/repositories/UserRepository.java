e03c2bfc256e462780838146ecb7f92bff228f53
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-21 16:23:23 +0400

    Create new methods

--- src/main/java/org/pronet/app/repositories/UserRepository.java
+++ src/main/java/org/pronet/app/repositories/UserRepository.java
@@ -1,14 +1,17 @@
 package org.pronet.app.repositories;
 
 import org.pronet.app.entities.User;
+import org.pronet.app.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameContainingIgnoreCase(String username);
+    Optional<User> findByUsernameContainingIgnoreCase(String username);
     Boolean existsByUsernameContainingIgnoreCase(String username);
+    Boolean existsByRole(Role role);
     List<User> findAllByUsernameContainingIgnoreCaseAndIdNot(String username, Long id);
 }
