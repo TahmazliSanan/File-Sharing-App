b4f75566857005b607993b08318052b14b5de2b1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2024-10-21 16:34:09 +0400

    Create SecurityConfig

---
+++ src/main/java/org/pronet/app/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package org.pronet.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.pronet.app.auth.AuthRequestMatcher;
+import org.pronet.app.auth.JwtFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AuthRequestMatcher.NON_AUTH_ROUTES).permitAll()
+                        .requestMatchers(AuthRequestMatcher.ADMIN_AUTH_ROUTES).hasAuthority("Admin")
+                        .requestMatchers(AuthRequestMatcher.USER_AUTH_ROUTES).hasAuthority("User")
+                        .requestMatchers(AuthRequestMatcher.ADMIN_AND_USER_AUTH_ROUTES).hasAnyAuthority("Admin", "User")
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
